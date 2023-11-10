32a3fa97329563366452d5d5399521709c59ced0
marrgarett <sb6560259101@lru.ac.th>
2023-11-10 11:54:17 +0700

    test commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          นาย กฤษณะ ตันติพงษ์
+          Kritsana Tuntipong
         </p>
         <a
           className="App-link"
